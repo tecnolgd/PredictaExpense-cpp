039d95a425383484dd6f6e11ab68f21643fc65a4
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-08-06 14:20:16 +0530

    ADD: head.hpp file

---
+++ head.hpp
@@ -0,0 +1,13 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+// Function declarations
+void dailyExpLog();
+void saveToFile();
+void weekLog();
+void savePredict();
+void dispHistory();
+void spendWarn();
+int menu();
+
+#endif
