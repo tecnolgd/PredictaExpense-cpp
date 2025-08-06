3ec06b15ea7053c58aeb2c20afe7f064deeb7091
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-08-06 14:19:10 +0530

    MODIFY: main.cpp file

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include "head.hpp"
+
+void dailyExpLog();
+void saveToFile();
+void weekLog();
+void savePredict();
+void dispHistory();
+void spendWarn();
+int menu();
+int main(){
+    int choice= menu(choice);
+
+    switch(choice){
+        case 1:void dailyExpLog();
+            break;
+        case 2:void saveToFile();
+            break;
+        case 3:void weekLog();
+            break;
+        case 4:void savePredict();
+            break;
+        case 5:void dispHistory();
+            break;
+        case 6:void spendWarn();
+            break;
+        default:
+            std::cout<<"invalid choice"<<'\n';
+        
+    }
+    return 0;
+
+}
