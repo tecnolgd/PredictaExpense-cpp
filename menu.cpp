b98a7c404e0d7fc3f2e47402e10cc559db61129e
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-08-06 14:52:58 +0530

    MODIFY: menu.cpp

---
+++ menu.cpp
@@ -0,0 +1,19 @@
+#include "head.hpp"
+#include <iostream>
+
+void menu(){
+    int choice;
+    std::cout<<'\t'<<"Welcome to the predictaexpense"<<'\n';
+    std::cout<<"-----------------"<<'\n';
+    std::cout<<"1.Planned budget"<<'\n';
+    std::cout<<"2.Add expenses"<<'\n';
+    std::cout<<"3.Weekly summary"<<'\n';
+    std::cout<<"4.View history file"<<'\n';
+    std::cout<<"5.End-Of-Month prediction"<<'\n';
+    std::cout<<"6.Insights and Strategies"<<'\n';std::cout<<"-----------------"<<'\n';
+
+
+
+
+    
+}
