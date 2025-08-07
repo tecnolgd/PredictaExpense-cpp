d99b1f2a99976536cc309edd73d6bd530ed17855
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-08-07 13:28:57 +0530

    MODIFY: menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -2,15 +2,15 @@
 #include <iostream>
 
 void menu(){
-    int choice;
-    std::cout<<'\t'<<"Welcome to the predictaexpense"<<'\n';
+    std::cout<<'\t'<<"Welcome to the PREDICTA EXPENSE"<<'\n';
     std::cout<<"-----------------"<<'\n';
-    std::cout<<"1.Planned budget"<<'\n';
+    std::cout<<"1.User budget"<<'\n';
     std::cout<<"2.Add expenses"<<'\n';
     std::cout<<"3.Weekly summary"<<'\n';
-    std::cout<<"4.View history file"<<'\n';
-    std::cout<<"5.End-Of-Month prediction"<<'\n';
-    std::cout<<"6.Insights and Strategies"<<'\n';std::cout<<"-----------------"<<'\n';
+    std::cout<<"4.View history"<<'\n';
+    std::cout<<"5.Insights and Strategies"<<'\n';
+    std::cout<<"6.Exit"<<'\n';
+    std::cout<<"-----------------"<<'\n';
 
 
 
