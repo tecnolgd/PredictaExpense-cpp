6161e54b9f3cc95e88de9a7476b553f75aa5158b
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-08-09 10:23:38 +0530

    MODIFY: menu.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 #include <string>
 #include "head.hpp"
-#include "menu.cpp"
-#include "userBudget.cpp"
-#include "history.cpp"
-#include "dailyLog.cpp"
-#include "insights.cpp"
-#include "warning.cpp"
-#include "weekLog.cpp"
+/**/
 
 
     /*int askBudget();
@@ -18,34 +12,45 @@
     void insights();
     void menu();*/
 int main(){
-    int choice,budget;
+    int choice=0,test=1;
+    double budget;
+    bool x=true;
     
     
-    do{
-        menu();
-        std::cout<<"Enter your choice :";
-        std::cin>>choice;
-
-    switch(choice){
-        case 1: budget= askBudget();
-            break;
-        case 2: dailyExpLog();
-            break;
-        case 3:warning();
-            break;
-        case 4:weekLog();
-            break;
-        case 5:dispHistory();
-            break;
-        case 6: insights();
-            break;
-        default:
-            std::cout<<"Invalid choice"<<'\n';
+    do{ 
         
-    }
-    }while(choice !=0);
+        menu();
+        if(test==0){
+            std::cout<<"Invalid choice.TRY AGAIN.o.\n";
+            test=1;
+        }
+        std::cout<<"Enter your choice :"<<std::endl;
+        std::cin>>choice;
+        
+        switch(choice){
+            case 1: budget= askBudget();
+                break;
+            case 2: dailyExpLog();
+                break;
+            case 3:warning();
+                break;
+            case 4:weekLog();
+                break;
+            case 5:dispHistory();
+                break;
+            case 6: insights();
+                break;
+            case 7: x=false;
+                break;
+            default : test=0;
+                break;
+         }  
+
+    }while(x);
+
     std::cout<<"Thank you, have a nice day."<<'\n';
     std::cout<<"-----------------"<<'\n';
+    
     return 0;
 
 }
