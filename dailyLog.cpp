1ef809ee73562459466bca55da0ea847fcbde83f
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-08-10 12:48:37 +0530

    MODIFY: dailyLog.cpp

--- dailyLog.cpp
+++ dailyLog.cpp
@@ -1,5 +1,31 @@
 #include "head.hpp"
-void dailyExpLog(){
+#include <iostream>
+#include <string>
+void dailyExpLog()
+{
+    std::string details;
+    int log_id=1,i=1;
+    std::cout<<"  ** DAILY LOG **  \n";
+    std::cout<<"-----------------"<<'\n';
+    std::cout<<"Please enter expense details :\n";
+    std::cout<<"( NOTE: Detalis must include date,expense source and total expense )\n";
+
+    while(log_id == 1)
+    {   
+        std::cout<<i<<") ";
+        std::cin.ignore(); // to avoid skipping input
+        std::getline(std::cin,details);
+        std::cout<<"\nEnter :0 to complete the log, 1 to continue logging.\t";
+        std::cin>>log_id;
+        i++;
+
+    }
+}
+    
+    
+    
+   
+
+
     
 
-}
