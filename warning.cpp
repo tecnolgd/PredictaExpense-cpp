88958e2e687eb4615cb450d4d69db76e8aff0fb2
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-08-13 10:02:34 +0530

    MODIFY: warning.cpp file

--- warning.cpp
+++ warning.cpp
@@ -1,17 +1,20 @@
 #include "head.hpp"
 #include <iostream>
 
-char warning(int bud,int exp){
+void warning(int bud,int exp){
+
     if(bud<exp){
         std::cout<<"Warning : You are overspending !\n";
-        return 'H';
+        
     }
-    else {
-        if(bud==exp){
+    else if(bud==exp){
             std::cout<<"Warning : Your net budget today is ZERO !\n";
-            return 'L';
+            
         }
+    else{
+        std::cout<<"Well done !.yoour savinga are progressing.\n";
     }
+    
 }
     
 
