ed20a38290a5c109e7b7b705ad2883bbd75ab626
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-08-15 15:35:25 +0530

    MODIFY: warning.cpp file

--- warning.cpp
+++ warning.cpp
@@ -1,20 +1,20 @@
 #include "head.hpp"
 #include <iostream>
 
-void warning(double bud,double exp){
-
+void warning(double bud,double exp)
+{
+    std::cout<<"____________________________\n";
     if(bud<exp){
         std::cout<<"Warning : You are overspending !\n";
-        
     }
     else if(bud==exp){
             std::cout<<"Warning : Your net budget today is ZERO !\n";
-            
-        }
+
+    }
     else{
         std::cout<<"Well done !.yoour savinga are progressing.\n";
     }
-    
+    std::cout<<"____________________________\n\n";
 }
     
 
