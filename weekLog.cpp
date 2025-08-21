130151bee74500d5b82f82fcc21ca2649f89a211
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-08-21 11:37:01 +0530

    MODIFY: weekLog.cpp file

--- weekLog.cpp
+++ weekLog.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include "head.hpp"
 
-void weekLog(){
+void weekLog(int session_counter){
+    if (session_counter==7){
+        std::cout<<"The weekly report is ready .....\n";
+        
+    }
+    else{
+        std::cout<<"The week is not over yet. "<<7-session_counter<<" log days left !!\n";
+    }
 
 }
