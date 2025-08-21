4213f9142d6f678e763ec691470729600a52fd29
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-08-21 10:48:27 +0530

    MODIFY: userBudget.cpp file

--- userBudget.cpp
+++ userBudget.cpp
@@ -1,5 +1,6 @@
 #include "head.hpp"
 #include <iostream>
+#include <fstream>
 
 double askBudget()
 {
@@ -18,8 +19,12 @@ double askBudget()
         }
         std::cout<<"\t--- enter 1 to confirm, 0 to change the budget ---\n";
         std::cin>>confirm;
+
     }
     while(confirm !=1);
+
+    std::ofstream file("expense_log.txt");
+    file<<"BUDGET: "<<budget<<std::endl; //reading budget to the file.
 
     std::cout<<"-----------------"<<'\n';
     return budget;
