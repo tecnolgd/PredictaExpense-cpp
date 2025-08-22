cdd6d884141b18ac7abc23a7bc01ad2bf45d71fd
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-08-22 11:13:59 +0530

    MODIFY: insights.cpp file

--- insights.cpp
+++ insights.cpp
@@ -1,6 +1,11 @@
 #include "head.hpp"
 #include <iostream>
-void insights(){
-    
 
+void insights(double saving,double budget){
+    std::cout<<"____________________________"<<std::endl;
+    std::cout<<"**  INSIGHTS  **\n";
+    std::cout<<"Monthly budget :"<<budget<<std::endl;
+    std::cout<<"Predicted savings for the month :"<<saving<<std::endl;
+    std::cout<<"____________________________"<<std::endl;
+    
 }
