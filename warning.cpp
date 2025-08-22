fb7094140465ddac59eb1b606ee9c8060eae469c
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-08-22 10:23:39 +0530

    MODIFY: warning.cpp file

--- warning.cpp
+++ warning.cpp
@@ -2,18 +2,19 @@
 #include <iostream>
 
 void warning(double bud,double exp)
-{
-    std::cout<<"____________________________\n";
-    if(bud<exp){
+{   double predict_save;
+    predict_save= bud-(exp*30.5); //prediction of budget logic / avg days= 30+31 days =30.5
+    std::cout<<"____________________________"<<std::endl;
+    if(predict_save<0.0){
         std::cout<<"Warning : You are overspending !\n";
     }
     else if(bud==exp){
             std::cout<<"Warning : Your net budget today is ZERO !\n";
     }
     else{
-        std::cout<<"Well done !.yoour savinga are progressing.\n";
+        std::cout<<"Well done ! You are on the way to save "<<predict_save<<"by the end of the month\n";
     }
-    std::cout<<"____________________________\n\n";
+    std::cout<<"____________________________"<<std::endl;
 }
     
 
