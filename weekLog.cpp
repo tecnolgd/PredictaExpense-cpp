db22085e7d869a8323800ff3c2ab115c3008403d
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-08-23 09:28:47 +0530

    MODIFY: weekLog.cpp file

--- weekLog.cpp
+++ weekLog.cpp
@@ -1,24 +1,23 @@
+//This code computes the weekky log display function with week 
 #include <iostream>
 #include <fstream>
 #include "head.hpp"
 
 void weekLog(int session_counter){
-    if (session_counter==7){
+    if (session_counter==7){ // weekly report generates only after completing 7 days or a week
         std::cout<<"The weekly report is ready .....\n";
         std::ifstream file("expense_log.txt");
         if(file.is_open()){
             std::string line;
             while(std::getline(file,line)){      //to print content from the text file to the console.
                 std::cout<<line<<std::endl;
-
             }
             file.close();
         } else{
-            std::cout<<"Unable to open the file.\n";
+            std::cout<<"UNABLE to open the file.\n";
         }
     }
     else{
-        std::cout<<"The week is not over yet. "<<7-session_counter<<" log days left !!\n";
+        std::cout<<"The week is NOT over yet. "<<7-session_counter<<" log days left !!\n"; //logic statement in case the user requests week log before a week is completed
     }
-
 }
