76119f8f82f5d81cf010edc57624f4e7bdd037e3
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-08-23 19:05:07 +0530

    MODIFY: menu.cpp file

--- menu.cpp
+++ menu.cpp
@@ -9,7 +9,7 @@ void menu(){
     std::cout<<"| [2]--- Add EXPENSE                |"<<'\n';
     std::cout<<"| [3]--- WARNINGS                   |"<<'\n';
     std::cout<<"| [4]--- Weekly LOG                 |"<<'\n';
-    std::cout<<"| [5]--- LOG history                |"<<'\n';
+    std::cout<<"| [5]---  All time LOG history      |"<<'\n';
     std::cout<<"| [6]--- Insights AND Strategies    |"<<'\n';
     std::cout<<"| [7]--- EXIT                       |"<<'\n';
     std::cout<<"|___________________________________|"<<'\n';
