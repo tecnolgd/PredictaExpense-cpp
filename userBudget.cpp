3b1b91131f0cdd24aaf59c2ec89152dd14f53810
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-08-23 08:50:30 +0530

    MODIFY: userBudget.cpp file

--- userBudget.cpp
+++ userBudget.cpp
@@ -1,3 +1,4 @@
+//This code reads budget from from the user and writes it to a text file.
 #include "head.hpp"
 #include <iostream>
 #include <fstream>
@@ -10,7 +11,7 @@ double askBudget()
     std::cout<<"$   BUDGET SETTINGS   $  \n";
     std::cout<<"-----------------"<<'\n';
 
-    do{
+    do{                                           //loop for budget entry and change as per user demands
         std::cout<<"Please enter your  monthly budget :"<<'\n';
         std::cin>>budget;
         while(budget<=0.0){
@@ -19,12 +20,11 @@ double askBudget()
         }
         std::cout<<"\t--- enter 1 to confirm, 0 to change the budget ---\n";
         std::cin>>confirm;
-
     }
     while(confirm !=1);
 
-    std::ofstream file("expense_log.txt");
-    file<<"MONTHLY BUDGET: "<<budget<<std::endl; //reading budget to the file.
+    std::ofstream file("expense_log.txt"); //open file 
+    file<<"MONTHLY BUDGET: "<<budget<<std::endl; // writing budget to the file.
     std::cout<<"-----------------"<<'\n';
     return budget;
 }
