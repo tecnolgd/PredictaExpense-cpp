f4a5356a9b9c082a44a32bde8b99aeba9062eba3
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-08-23 09:23:19 +0530

    MODIFY: menu.cpp file

--- menu.cpp
+++ menu.cpp
@@ -2,20 +2,14 @@
 #include <iostream>
 
 void menu(){
-    
     std::cout<<'\t'<<"**   Welcome to the PREDICTA EXPENSE   **"<<'\n';
-    std::cout<<"-----------------"<<'\n';
-    std::cout<<"1.User budget"<<'\n';
-    std::cout<<"2.Add expenses"<<'\n';
-    std::cout<<"3.Warnings"<<'\n';
-    std::cout<<"4.Weekly Log"<<'\n';
-    std::cout<<"5.View history"<<'\n';
-    std::cout<<"6.Insights and Strategies"<<'\n';
-    std::cout<<"7.Exit"<<'\n';
-    std::cout<<"-----------------"<<'\n';
-
-
-
-
-    
+    std::cout<<"+-----------------------------------+"<<'\n';
+    std::cout<<"| [1]--- User BUDGET                |"<<'\n';
+    std::cout<<"| [2]--- Add EXPENSE                |"<<'\n';
+    std::cout<<"| [3]--- WARNINGS                   |"<<'\n';
+    std::cout<<"| [4]--- Weekly LOG                 |"<<'\n';
+    std::cout<<"| [5]--- LOG history                |"<<'\n';
+    std::cout<<"| [6]--- Insights AND Strategies    |"<<'\n';
+    std::cout<<"| [7]--- EXIT                       |"<<'\n';
+    std::cout<<"|___________________________________|"<<'\n';
 }
