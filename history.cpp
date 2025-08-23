15744f0ceb3819c0864f6b4e3c6cacc2ff02ddcf
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-08-23 08:49:03 +0530

    MODIFY: history.cpp file

--- history.cpp
+++ history.cpp
@@ -1,6 +1,4 @@
-// To display log history
-
-
+// To display log history to the console.
 #include "head.hpp"
 #include <iostream>
 #include <fstream>
@@ -10,14 +8,14 @@ void dispHistory(){
     std::cout<<"**  LOG HISTORY  **"<<std::endl;
     std::cout<<"--------------------"<<std::endl;
     std::ifstream file("expense_log.txt");
-        if(file.is_open()){
-            std::string line;
-            while(std::getline(file,line)){      //to print content from the text file to the console.
-                std::cout<<line<<std::endl;
-            }
-            file.close();
-        } else{
-            std::cout<<"Unable to open the file.\n";
-            }
+    if(file.is_open()){
+        std::string line;
+        while(std::getline(file,line)){      //to print content from the text file to the console.
+            std::cout<<line<<std::endl;
+        }
+        file.close();
+    } else{
+        std::cout<<"Unable to open the file.\n";
+        }
     std::cout<<"____________________________"<<std::endl;
 }
