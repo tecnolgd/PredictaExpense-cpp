f5022e945b39a1736954dd32ba8b605f21a1f7c2
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-08-23 08:47:31 +0530

    MODIFY: warning.cpp file

--- warning.cpp
+++ warning.cpp
@@ -1,17 +1,17 @@
+//This code generates warnings with relevant data if applicable
 #include "head.hpp"
 #include <iostream>
 
 double warning(double bud,double exp,int day_pass) //day_pass = no of days passed
 {  
     double avg_save=exp/day_pass;
-    double predict_save = bud-(avg_save*30.5); //prediction logic
+    double predict_save = bud-(avg_save*30.5); //savings prediction logic
     std::cout<<"____________________________"<<std::endl;
     if(predict_save<0.0){
         std::cout<<"Warning : You are overspending ! Projectect to overshoot the budget by "<<predict_save<< "this month"<<std::endl;
     }
     else{
         std::cout<<"Well done ! You are on the way to save "<<predict_save<<" by the end of the month\n";
-        
     }
     std::cout<<"____________________________"<<std::endl;
     return predict_save;
