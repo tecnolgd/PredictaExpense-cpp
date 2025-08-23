28c0204d1f89459e4d644a318a0f948c9ee38da9
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-08-23 08:47:16 +0530

    MODIFY: insights.cpp file

--- insights.cpp
+++ insights.cpp
@@ -1,3 +1,4 @@
+// This code does the insights and strategies computation.
 #include "head.hpp"
 #include <iostream>
 
@@ -14,15 +15,14 @@ void insights(double saving,double budget,int days){
     double target,diff;
     std::cout<<"Enter target saving : ";
     std::cin>>target;
-    diff=target-saving;
+    diff=target-saving; //difference b/w target and savings
     if(target>saving){
+        std::cout<<"* STRATEGIES *\n";
         std::cout<<"You are short of "<<diff<<" to reach you savings goal.."<<std::endl;
-        std::cout<<"You need to save "<< diff/(30-days)<<" per day to stay on track."<<std::endl;
+        std::cout<<"You need to save "<< diff/(30-days)<<" per day to stay on track."<<std::endl; // savings plan for the remaining days
     }
     else{
-        std::cout<<"Congratulations ! You have won BIG by saving an extra "<<abs(diff)<<" !!"<<std::endl;
+        std::cout<<"Congratulations ! You have won BIG by saving an extra "<<abs(diff)<<" than your taget savings!!"<<std::endl;
     }
     std::cout<<"____________________________"<<std::endl;
-
-    
 }
